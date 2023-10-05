026ea429e90ced848d0e8c5c7b526e18795475fc
Chris <ckatanic@gmail.com>
2023-10-04 20:55:47 -0700

    add astrix to required fields that are invalid

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const passwordInput = document.getElementById('password');
 const verifyPasswordInput = document.getElementById('verifyPassword');
 const passwordDiv = document.querySelector(".password");
+const button = document.querySelector(".button");
 
-passwordInput.addEventListener('change', verifyPasswords);
-verifyPasswordInput.addEventListener('change', verifyPasswords);
+passwordInput.addEventListener('input', verifyPasswords);
+verifyPasswordInput.addEventListener('input', verifyPasswords);
 
 function verifyPasswords() {
     if (passwordInput.value === verifyPasswordInput.value &&
         passwordInput.value !== "" &&
         verifyPasswordInput.value !== "") {
+        button.disabled=false;
         passwordInput.setAttribute("data-error", false);
         verifyPasswordInput.setAttribute("data-error", false);
         passwordDiv.setAttribute("data-error", false);
     } else {
+        button.disabled=true;
         passwordInput.setAttribute("data-error", true);
         verifyPasswordInput.setAttribute("data-error", true);
         passwordDiv.setAttribute("data-error", true);
