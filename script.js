70d6bfe11b94ceb41c9afdf70544c1d9ca1c768f
Chris <ckatanic@gmail.com>
2023-10-04 18:18:02 -0700

    add javascript to check for matching passwords and style accordingly

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Testing...")
+const passwordInput = document.getElementById('password');
+const verifyPasswordInput = document.getElementById('verifyPassword');
+const passwordDiv = document.querySelector(".password");
+
+passwordInput.addEventListener('change', verifyPasswords);
+verifyPasswordInput.addEventListener('change', verifyPasswords);
+
+function verifyPasswords() {
+    if (passwordInput.value === verifyPasswordInput.value &&
+        passwordInput.value !== "" &&
+        verifyPasswordInput.value !== "") {
+        passwordInput.setAttribute("data-error", false);
+        verifyPasswordInput.setAttribute("data-error", false);
+        passwordDiv.setAttribute("data-error", false);
+    } else {
+        passwordInput.setAttribute("data-error", true);
+        verifyPasswordInput.setAttribute("data-error", true);
+        passwordDiv.setAttribute("data-error", true);
+
+    }
+}
+
